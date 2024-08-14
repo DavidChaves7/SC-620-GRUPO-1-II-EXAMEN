79d0baf2ad6a1341795e41ce8f6b9f4b0e066532
DavidZ07 <dchavesd.07@gmail.com>
2024-08-13 21:10:12 -0600

    World Building y Menus
    Cosas basicas para el examen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager _instance;
+
+    public static LevelManager Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    public void FirstLevel()
+    {
+        int level = 0;
+        LoadLevel(level);
+    }
+
+    public void NextLevel()
+    {
+        int level = SceneManager.GetActiveScene().buildIndex + 1;
+        if (level > SceneManager.sceneCountInBuildSettings - 1)
+        {
+            level = SceneManager.sceneCountInBuildSettings - 1;
+        }
+        LoadLevel(level);
+    }
+
+    public void LastLevel()
+    {
+        int level = SceneManager.sceneCountInBuildSettings - 1;
+        LoadLevel(level);
+    }
+    private void LoadLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
